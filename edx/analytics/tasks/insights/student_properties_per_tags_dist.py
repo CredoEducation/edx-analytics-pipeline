12d4636e6603109a6c85ed19d3541ab967af9058
Dmitry Viskov <dmitry.viskov@webenterprise.ru>
2018-08-07 15:58:20 +0300

    DEBUG

--- edx/analytics/tasks/insights/student_properties_per_tags_dist.py
+++ edx/analytics/tasks/insights/student_properties_per_tags_dist.py
@@ -131,6 +131,9 @@ class StudentPropertiesPerTagsPerCourse(StudentPropertiesPerTagsPerCourseDownstr
             return
 
         user_id = event.get('context').get('user_id', None)
+        if user_id in [1108290, 1104604]:
+            print 'debug log: ',  line
+
         if not user_id:
             return
 
