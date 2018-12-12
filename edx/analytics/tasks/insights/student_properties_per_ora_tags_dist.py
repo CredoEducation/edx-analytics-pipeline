1f87942854cc5634ba26b0da1c89077b2e0cf706
Dmitry <dmitry.viskov@webenterprise.ru>
2018-12-12 15:45:06 +0300

    ET-719 - Replace manual terms configuration in Studio with a monthly filter in Insights (based on timestamp)

--- edx/analytics/tasks/insights/student_properties_per_ora_tags_dist.py
+++ edx/analytics/tasks/insights/student_properties_per_ora_tags_dist.py
@@ -69,6 +69,10 @@ class StudentPropertiesPerOraTagsPerCourse(
         if timestamp is None:
             return
 
+        dtime = eventlog.get_event_time(event)
+        if dtime is None:
+            return
+
         course_id = eventlog.get_course_id(event)
         if not course_id:
             return
@@ -105,8 +109,9 @@ class StudentPropertiesPerOraTagsPerCourse(
                 if new_value:
                     overload_items[k]['value'], student_properties = new_value, new_properties
 
-        if overload_items['term']['value']:
-            student_properties['enrollment']['terms'] = overload_items['term']['value']
+        #if overload_items['term']['value']:
+        #    student_properties['enrollment']['terms'] = overload_items['term']['value']
+        student_properties['enrollment']['terms'] = dtime.strftime("%B %Y")
 
         question_text = u''
         prompts_list = []
